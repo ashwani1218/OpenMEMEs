40db24e65524523eb992dad6535715b9efce6976
vcothur7@gmail.com <vcothur7@gmail.com>
2019-09-08 19:17:04 +0530

    implemented Google logout functionality, closes #12

---
+++ static/js/home.js
@@ -0,0 +1,14 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+
+        // TODO: Change the url to home after session is implemented.
+        window.open("/","_self");
+    });
+}
+function gapiInit(){
+    gapi.load('auth2', function() {
+        gapi.auth2.init()  /* Ready. Make a call to gapi.auth2.init or some other API */
+    });    
+}
