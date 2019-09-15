130be87724a6aaacf8996870cc835fd1b4c469b7
vcothur7@gmail.com <vcothur7@gmail.com>
2019-09-15 20:27:54 +0530

    added favicon, closes #24

--- main.py
+++ main.py
@@ -163,4 +163,7 @@ def page_not_found(error):
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000,debug=True)
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
